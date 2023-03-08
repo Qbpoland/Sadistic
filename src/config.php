3cd93e280192305ba98e639aaf0dd41b8a0373fa
Qbpoland <jakub.stasiuk2005@gmail.com>
2023-03-08 12:13:51 +0100

    all work

--- src/config.php
+++ src/config.php
@@ -5,4 +5,8 @@ require("./../vendor/autoload.php");
 require('./../src/Post.class.php');
 $db = new mysqli('localhost', 'root', '','cms_js');
 
+$loader = new Twig\Loader\FilesystemLoader('./../src/templates');
+//inicjujemy twiga
+$twig = new Twig\Environment($loader);
+
 ?>
